e2e79916c21faa190e2be77946bc95321f56adcd
Kantimahanty Rohit <61279841+Kay-Rohit@users.noreply.github.com>
2020-06-02 19:26:17 +0530

    activity indicator

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { Tile } from 'react-native-elements';
 
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+
+import {Loading} from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -19,9 +21,6 @@ class Menu extends React.Component {
     };
 
     render() {
-
-        const {navigate} = this.props.navigation;
-
         const renderMenuItem = ({item, index}) => {
 
             return (
@@ -36,15 +35,32 @@ class Menu extends React.Component {
             );
         };
 
-        return (
-            <FlatList 
-                    data={this.props.dishes.dishes}
-                    renderItem={renderMenuItem}
-                    keyExtractor={item => item.id.toString()}
-                    />
-        );
-    }
+        const {navigate} = this.props.navigation;
 
+        if(this.props.dishes.isLoading){
+            return(
+                <Loading />
+            );
+        }
+        else if(this.props.dishes.errMess)
+        {
+            return(
+                <View>
+                    <Text>{this.props.dishes.errMess}</Text>
+                </View>
+            );
+        }
+        else
+        {
+            return (
+                <FlatList 
+                        data={this.props.dishes.dishes}
+                        renderItem={renderMenuItem}
+                        keyExtractor={item => item.id.toString()}
+                        />
+            );
+        }
+    }
 }
 
 
